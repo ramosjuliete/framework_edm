35f735bc5c4a3a42df22156cccfa9ab4905c895a
ramosjuliete <juliete.costa@ifsuldeminas.edu.br>
2025-04-03 16:46:48 -0300

    Ajustes dos métodos de normalidade de dados na classe ExploryAnalysis

--- framework_edm/exploratoyanalysis.py
+++ framework_edm/exploratoyanalysis.py
@@ -5,39 +5,45 @@ import os
 import seaborn as sns
 import matplotlib.pyplot as plt
 from scipy.stats import kstest
+import scipy.stats as stats
 
 class ExploratoryAnalysis:
     def __init__(self,path_sheet, separator):
         self.path_sheet = path_sheet
         self.separator = separator
 
+    #MÉTODO 1 -> Remove identificadores se houver
+    def removeIdentifierColumns(self, dataframe):
+        # Colunas a remover
+        remove_columns = ['iduser','name']
+        # Verifica e remove as colunas existentes
+        existing_columns = [col for col in remove_columns if col in dataframe.columns]
+        if existing_columns:
+            dataframe = dataframe.drop(columns=existing_columns)
+        return dataframe
 
-    #MÉTODO 1 -> Carregando o dataframe a partir do arquivo enviano no momento da criação do objeto (construtor)
+    #MÉTODO 2 -> Carregando o dataframe a partir do arquivo enviano no momento da criação do objeto (construtor)
     def loadDataframe(self):
         df = pd.read_csv(self.path_sheet, delimiter=self.separator)
         return df
 
-    #MÉTODO 2 -> Retornando informações do dataframe
+    #MÉTODO 3 -> Retornando informações do dataframe
     def informationData(self, dataframe):
         return dataframe.info()
 
-    #MÉTODO 3 -> Retornando descrições estatísticas do dataframe
+    #MÉTODO 4 -> Retornando descrições estatísticas do dataframe
     def statisticalDescription(self, dataframe):
         if 'name' in dataframe.columns:
             dataframe.drop('name', axis=1, inplace=True)
         return dataframe.describe()
     
-    #MÉTODO 4 -> Criando a matriz de correlação de Spearman -> quando os dados nao seguem uma distribuição normal
+    #MÉTODO 5 -> Criando a matriz de correlação de Spearman -> quando os dados nao seguem uma distribuição normal
     def createSpearmanMatrix(self, dataframe, path):
-        # Colunas a remover
-        remove_columns = ['iduser', 'name']
-        # Verifica e remove as colunas existentes
-        existing_columns = [col for col in remove_columns if col in dataframe.columns]
-        if existing_columns:
-            dataframe = dataframe.drop(columns=existing_columns)
+
+        df = self.removeIdentifierColumns(dataframe)
 
         # Calcula a matriz de correlação utilizando o método Spearman
-        corr = dataframe.corr(method='spearman')
+        corr = df.corr(method='spearman')
     
         # Configuração do estilo (apenas para visualização em Jupyter ou exportação em HTML)
         styled_corr = corr.style.background_gradient(cmap='coolwarm')
@@ -52,30 +58,49 @@ class ExploratoryAnalysis:
         plt.show()
     
 
-    # MÉTODO 5 -> Aplica o teste de normalidade de dados do Kolmogorov Smirnov
-    # Ideal para amostras acima de 5000 registros
-    
-    def aplicar_kstest(self, coluna):
-        estatistica, p_valor = kstest(coluna, 'norm')
-        return estatistica, p_valor
+    # MÉTODO 6 -> Aplica o teste de normalidade de dados do Shapiro Wilk - Ideal para dados com até 5000 amostras
+    def shapiro_wilk_test(self, dataframe):
+        results = []
+        for column in dataframe.columns:
+            stat, p_value = stats.shapiro(dataframe[column])
+            normal = "✅ Yes" if p_value > 0.05 else "❌ Not"
+            results.append([column, stat, p_value, normal])
+        return pd.DataFrame(results, columns=["Attribute", "Statistic", "p-value", "Normal Distribution?"])
 
-    # MÉTODO 6 -> Faz a chamada do teste de normalidade 
-    # Para dados abaixo de 5000 --> aplicar Shapiro Wilk (verificar se não preciso implementar este), talvez passar por parametro "shapiro", "smirnov" e "anderson" para escolha um dos testes
-    def applyNormalityTest(self, dataframe):
-        #if 'iduser' in dataframe.columns:
-            #dataframe.drop('iduser', axis=1, inplace=True)
-        resultados = {}
-        for coluna in dataframe.select_dtypes(include=[np.number]).columns:
-            estatistica, p_valor = self.aplicar_kstest(dataframe[coluna])
-            resultados[coluna] = {'estatistica': estatistica, 'p_valor': p_valor}
+    # MÉTODO 7 -> Aplica o teste de normalidade de dados do Kolmogorov Smirnov 
+    def kolmogorov_smirnov_test(self, dataframe):
+        results = []
+        for column in dataframe.columns:
+            stat, p_value = stats.kstest(dataframe[column], 'norm')
+            normal = "✅ Yes" if p_value > 0.05 else "❌ Not"
+            results.append([column, stat, p_value, normal])
+        return pd.DataFrame(results, columns=["Attribute", "Statistic", "p-value", "Normal Distribution?"])
 
-        print(f"\n******************* Results of Kolmogorov-Smirnov Test *******************\n")
-    
-        for coluna, resultado in resultados.items():
-            estatistica = resultado['estatistica']
-            p_valor = resultado['p_valor']
-            #print(f"{coluna}: statistic = {estatistica}, p_value = {p_valor:.6f}")
-            #TROCAR PELAS LINHAS ABAIXO PARA MOSTRAR A FRASE DE NORMALIDADE
-            normality = "Follows a normal distribution" if p_valor > 0.05 else "Does not follow a normal distribution"
-            print(f"{coluna}: statistic = {estatistica}, p_value = {p_valor:.6f} → {normality}")
-        print('\n')
+    # MÉTODO 8 -> Aplica o teste de normalidade de dados do Anderson-Darling
+    def anderson_darling_test(self, dataframe):
+        results = []
+        for column in dataframe.columns:
+            test_result = stats.anderson(dataframe[column], dist='norm')
+            stat = test_result.statistic
+            critical_value = test_result.critical_values[2]  # Pegando o valor crítico para 5%
+            normal = "✅ Yes" if stat < critical_value else "❌ Not"
+            results.append([column, stat, critical_value, normal])
+        return pd.DataFrame(results, columns=["Variable", "Statistic", "Critical Value (5%)", "Normal Distribution?"])
+   
+    # MÉTODO 7 -> Faz a chamada do teste de normalidade que o usuário passar por parâmetro (smirnov, shapiro ou anderson) 
+    def applyNormalityTest(self, dataframe, test):
+        df = self.removeIdentifierColumns(dataframe)
+        df_result = pd.DataFrame()
+        if(test=='smirnov'):
+            df_result = self.kolmogorov_smirnov_test(df)
+            print(f"\n******************* Results of Kolmogorov-Smirnov Test *******************\n")
+        elif(test=='shapiro'):
+            df_result = self.shapiro_wilk_test(df)
+            print(f"\n******************* Results of Shapiro-Wilk Test *******************\n")
+        elif(test=='anderson'):
+            df_result = self.anderson_darling_test(df)
+            print(f"\n******************* Results of Anderson-Darling Test *******************\n")
+        else:
+            print(f'Error. Test not available!')
+            return
+        print(df_result)
